31fe5acd562d1460a6bddea9ca20acf731fd3774
Zyo86 <zyothebest86@gmail.com>
2021-09-26 10:06:25 +0530

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from "./App";
+import Example from './App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Example />, document.getElementById('root'));
